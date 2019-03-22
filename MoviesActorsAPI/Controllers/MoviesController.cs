f58596b685c3d997743b0bfa4a8542b1767d6c73
philyt2016 <x00044772@gmail.com>
2019-03-22 20:06:19 +0000

    delete and post added

--- MoviesActorsAPI/Controllers/MoviesController.cs
+++ MoviesActorsAPI/Controllers/MoviesController.cs
@@ -31,14 +31,27 @@ namespace MoviesActorsAPI.Controllers
             return entries;
         }
 
-        
+        // GET api/movies/name/
+        [HttpGet("name/{name}", Name = "GetMovieByName")]        
+        public IActionResult GetMovieByName(string name)
+        {
+            // LINQ query, find matching entry for number
+            var entry = _context.Movie.FirstOrDefault(m => m.Name.ToUpper() == name.ToUpper());
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            return Ok(entry);
+        }
+
+
 
         //GET: api/movies/genre/eg. action
-        [HttpGet("genre/{Genre}")]
-        public IActionResult GetMovieByGenre(String Genre)
+        [HttpGet("genre/{genre}")]
+        public IActionResult GetMovieByGenre(String genre)
         {
             // LINQ query, find matching entries for genre
-            var entries = _context.Movie.Where(r => r.Genre.ToUpper() == Genre.ToUpper());
+            var entries = _context.Movie.Where(r => r.Genre.ToUpper() == genre.ToUpper());
 
             if (entries == null)
             {
@@ -47,11 +60,12 @@ namespace MoviesActorsAPI.Controllers
             return Ok(entries);
         }
 
-        [HttpGet("stars/{Stars:int}")]
-        public IActionResult GetMovieByStars(int Stars)
+
+        [HttpGet("stars/{stars:int}")]
+        public IActionResult GetMovieByStars(int stars)
         {
             // LINQ query, find matching entries for num of stars
-            var entries = _context.Movie.Where(r => r.Stars == Stars);
+            var entries = _context.Movie.Where(r => r.Stars == stars);
 
             if (entries == null)
             {
@@ -60,11 +74,12 @@ namespace MoviesActorsAPI.Controllers
             return Ok(entries);
         }
 
-        [HttpGet("year_released/{ReleaseYear:int}")]
-        public IActionResult GetMovieByReleaseYear(int ReleaseYear)
+
+        [HttpGet("year_released/{release_year:int}")]
+        public IActionResult GetMovieByReleaseYear(int release_year)
         {
             // LINQ query, find matching entries for num of stars
-            var entries = _context.Movie.Where(r => r.ReleaseYear == ReleaseYear);
+            var entries = _context.Movie.Where(r => r.ReleaseYear == release_year);
 
             if (entries == null)
             {
@@ -73,6 +88,59 @@ namespace MoviesActorsAPI.Controllers
             return Ok(entries);
         }
 
+
+        // DELETE: api/movies/name
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteMovie([FromRoute] string name)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var movie = await _context.Movie.FindAsync(name);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return Ok(movie);
+        }
+
+
+        // POST: api/movies
+        // add an entry
+        [HttpPost]
+        [ProducesResponseType(201)]             // created
+        [ProducesResponseType(400)]             // bad request
+        public IActionResult PostAddEntry([FromBody] Movie movie)
+        {
+            if (ModelState.IsValid)                                             // model class validation ok?
+            {
+                // check for duplicate
+                // LINQ query - get record
+                var record = _context.Movie.SingleOrDefault(e => e.Name.ToUpper() == movie.Name.ToUpper());
+                if (record == null)
+                {
+                    _context.Movie.Add(movie);
+                    _context.SaveChanges();                                           // commit
+
+                    // create http response with Created status code and listing serialised as content and Location header set to URI for new resource
+                    return CreatedAtRoute("GetMovieByName", new { name = movie.Name }, movie);
+                }
+                else
+                {
+                    return BadRequest("resource already exists");      // 400, already exists
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);        // 400
+            }
+        }
 
         //test
         private bool MovieExists(string id)
